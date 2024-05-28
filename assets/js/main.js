b2ce91de5842174813c1455ae7ba487cd7f17cc8
Nikhil Bishnoi <nikhilbishnoi@Nikhils-MacBook-Air.local>
2024-05-28 16:21:32 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+function openNav() {
+    document.getElementById("navbar").classList.toggle("start-0");
+    document.body.classList.toggle("overflow_hidden");
+    document.querySelector(".menu").classList.toggle("cross")
+  }
+
+
+
+$('.responsive_2').slick({
+  dots: false,
+  arrows: false,
+  infinite: true,
+  slidesToScroll: 1,
+  variableWidth: true,
+  slidesToShow: 3,
+  autoplay: true,
+  autoplaySpeed: 0,
+  speed: 2000,
+  pauseOnHover: true,
+  cssEase: 'linear',
+});
+ 
