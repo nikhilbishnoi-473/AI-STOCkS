15862e97bda6d107118a7fd1201f3cb8d4361f1e
Nikhil Bishnoi <nikhilbishnoi@Nikhils-MacBook-Air.local>
2024-05-30 15:25:41 +0530

    half section responsive done

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,34 @@ $('.responsive_2').slick({
   pauseOnHover: true,
   cssEase: 'linear',
 });
+
+
+$('.responsive1').slick({
+  dots: false,
+  arrows: false,
+  infinite: true,
+  slidesToScroll: 1,
+  variableWidth: true,
+  slidesToShow: 4,
+  autoplay: true,
+  autoplaySpeed: 0,
+  speed: 2000,
+  pauseOnHover: true,
+  cssEase: 'linear',
+  
+});
+$('.responsive').slick({
+  dots: false,
+  arrows: false,
+  infinite: true,
+  slidesToScroll: 1,
+  variableWidth: true,
+  slidesToShow: 4,
+  autoplay: true,
+  autoplaySpeed: 0,
+  speed: 2000,
+  pauseOnHover: true,
+  cssEase: 'linear',
+  rtl: true,
+   
+});
